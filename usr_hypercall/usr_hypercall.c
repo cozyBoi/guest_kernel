d5252de45d2b3c69701db31139544377d30c669a
j <kjjh1126@gmail.com>
2021-12-27 08:21:17 +0000

    change hypercall number

--- usr_hypercall/usr_hypercall.c
+++ usr_hypercall/usr_hypercall.c
@@ -5,9 +5,9 @@
 #include "usr_hypercall.h"
 #include <uapi/linux/kvm_para.h>
 #include <linux/cpumask.h>
-
+#define KVM_HC_USR_HYPERCALL            12
 
 asmlinkage long sys_usr_hypercall(void){
-	kvm_hypercall0(9);
+	kvm_hypercall0(KVM_HC_USR_HYPERCALL);
 	return 0;
 }
