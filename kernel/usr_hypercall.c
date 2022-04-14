5a4ff98fe5f318cbef293d3fbbdf3a64666f1702
root <root@localhost.localdomain>
2022-04-14 21:53:07 +0900

    change

--- kernel/usr_hypercall.c
+++ kernel/usr_hypercall.c
@@ -5,6 +5,7 @@
 //#include "usr_hypercall.h"
 #include <uapi/linux/kvm_para.h>
 #include <linux/cpumask.h>
+#include <linux/sched.h>
 
 //asmlinkage long sys_usr_hypercall(void){
 /*
@@ -15,8 +16,18 @@ SYSCALL_DEFINE0(hello){
 }
 */
 //asmlinkage long sys_hello(void){
+//argument $1 : pid
+//argument $2 : 0
 SYSCALL_DEFINE2(hello, long int, high_addr, long int, low_addr){
+	unsigned long long int tmp;
+	unsigned long int h, l;
+	pid_t curr_pid = high_addr;
+	struct task_struct *t = find_task_by_vpid(high_addr);
 	printk("hello world\n");
-	kvm_hypercall2(12, high_addr, low_addr);
+	tmp = (unsigned long long)t->mm->pgd / (1ULL<<32);
+	h = (unsigned long)tmp;
+	tmp = (unsigned long long)t->mm->pgd % (1ULL<<32);
+	l = (unsigned long)tmp;
+	kvm_hypercall3(13, h, l, curr_pid);	
 	return 0;
 }
